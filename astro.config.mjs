5f9550c57c65cf6459f8fd5adc73229d36660b9c
Ufuk Zobali <ufukzobali@Ordinateur-portable-de-Ufuk.local>
2025-07-18 16:05:04 +0200

    Correction de la configuration du sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // ↓↓↓ AJOUTEZ CETTE LIGNE ↓↓↓
+  // On retire les "//" pour que la ligne soit lue par le programme
   site: 'https://www.fiduciaire-expert.lu',
 
   integrations: [sitemap()]
